115c58604b30d840c778cbdfd08217c7788dc42a
kkreddyk <kkreddy.k@gmail.com>
2022-03-27 20:33:41 +0100

    working log4jworking.scala

---
+++ testlog4j/src/main/scala/testlog4j/log4jworking.scala
@@ -0,0 +1,35 @@
+package testlog4j
+
+import org.apache.log4j.LogManager
+import org.apache.log4j.Logger
+import org.apache.log4j.PropertyConfigurator
+import org.apache.log4j.Level
+
+object log4jworking {
+  
+  
+  def main(args: Array[String]){
+    
+    println("Testing log4j")
+    
+    val logger: Logger = LogManager.getLogger(this.getClass.getName)
+    
+    PropertyConfigurator.configure(args(0))
+     logger.info("Logger : Welcome to log4j::"+this.getClass.getName)
+
+	  logger.debug("This is debug message");
+	  logger.info("This is info message");
+	  logger.warn("This is warn message");
+	  logger.fatal("This is fatal message");
+	  logger.error("This is error message");
+	    logger.error("======");
+	  println("log level:::::"+ logger.getLevel())
+	  
+	  Logger.getLogger("org").setLevel(Level.OFF)
+    
+	  Logger.getLogger("akka").setLevel(Level.OFF)
+	  
+	  logger.info("Testing logger INFO")
+	  
+  }
+}
