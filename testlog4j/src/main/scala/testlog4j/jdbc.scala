eb649b4aff178227653ce4befe1bf115dec012dc
kkreddyk <kkreddy.k@gmail.com>
2022-03-28 21:30:52 +0100

    some changes to jdbc

--- testlog4j/src/main/scala/testlog4j/jdbc.scala
+++ testlog4j/src/main/scala/testlog4j/jdbc.scala
@@ -2,7 +2,16 @@ package testlog4j
 
 
 import org.apache.spark.sql.SparkSession
+
+import java.text.SimpleDateFormat
+import java.util.Date
 import java.util.Properties;
+
+import org.apache.log4j.LogManager
+import org.apache.log4j.Logger
+import org.apache.log4j.PropertyConfigurator
+import org.apache.log4j.Level
+
 object jdbc {
   
   
@@ -10,12 +19,21 @@ object jdbc {
    
    println("JDBC Start::")
    
-   val spark=SparkSession.builder.master("local[*]").getOrCreate()
+  
+   
+ val logger: Logger = LogManager.getLogger(this.getClass.getName)
+    
+     val dateFormat: SimpleDateFormat = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss");
+    System.setProperty("runid", dateFormat.format(new Date()));
+    
+    PropertyConfigurator.configure(args(0))
+     logger.info("Logger : Welcome to log4j::"+this.getClass.getName)
+    
+      val spark=SparkSession.builder.master("local[*]").getOrCreate()
    
    spark.sparkContext.setLogLevel("ERROR")
    spark.conf.getAll.foreach(println)
    
-
 val jdbcDF = spark.read
   .format("jdbc")
 .option("url", "jdbc:postgresql://localhost:5432/postgres")
@@ -31,9 +49,20 @@ val jdbcDF = spark.read
 connectionProperties.put("user", "postgres")
 connectionProperties.put("password", "postgres")
 
+val query="(select count(*) as cnt from public.t1 )q"
 
-val jdbcDF2 = spark.read.jdbc("jdbc:postgresql://localhost:5432/postgres","public.t1",connectionProperties)
+//val jdbcDF2 = spark.read.jdbc("jdbc:postgresql://localhost:5432/postgres","public.t1",connectionProperties)
  
+val jdbcDF2 = spark.read.jdbc("jdbc:postgresql://localhost:5432/postgres",query,connectionProperties)
+ 
+
 jdbcDF2.show(false)
+
+val target_table="public.t2"
+
+jdbcDF2.write.mode("append").option("truncate","true").jdbc("jdbc:postgresql://localhost:5432/postgres",target_table,connectionProperties)
+
+spark.read.jdbc("jdbc:postgresql://localhost:5432/postgres","public.t2",connectionProperties).show(false)
+ 
  }
 }
